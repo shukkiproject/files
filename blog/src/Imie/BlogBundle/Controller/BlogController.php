def4c0d00fdaf86e84ce04e7b46b5acc6c16dbd2
imie <imie@localhost.localdomain>
2016-03-28 11:25:15 +0200

    admin's users list

--- blog/src/Imie/BlogBundle/Controller/BlogController.php
+++ blog/src/Imie/BlogBundle/Controller/BlogController.php
@@ -28,7 +28,14 @@ class BlogController extends Controller
      */
     public function adminAction()
     {
-        return new Response('<html><body>Admin page!</body></html>');
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'Unable to access this admin page!');
+
+        $userManager = $this->get('fos_user.user_manager');
+        $users=$userManager->findUsers();
+
+        return $this->render('ImieBlogBundle:Blog:admin.html.twig', array('users' => $users,));
+        
+        // return new Response('<html><body>Admin page!</body></html>');
     }
 
      /**
@@ -36,41 +43,9 @@ class BlogController extends Controller
      */
     public function adminUsersAction()
     {
+        $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN', null, 'Unable to access this super admin page!');
+        
         return new Response('<html><body>Admin Users page!</body></html>');
     }
- //    public function ajouterAction()
- //    {
- //        // return $this->render('ImieBlogBundle:Default:index.html.twig');
- //        return new Response('ajouter');
- //    }
- //    public function modifierAction($id)
- //    {
- //        // return $this->render('ImieBlogBundle:Default:index.html.twig');
- //        return new Response('modifier'. $id);
- //    }
- //    public function supprimerAction($id)
- //    {
- //        // return $this->render('ImieBlogBundle:Default:index.html.twig');
- //        return new Response('supprimer'. $id);
- //    }
- //    public function voirAction($id)
- //    {
- //        // return $this->render('ImieBlogBundle:Default:index.html.twig');
- //        return new Response('voir'. $id);
- //    }
- //    public function ajCommentaireAction()
- //    {
- //        // return $this->render('ImieBlogBundle:Default:index.html.twig');
- //        return new Response('ajCommentaire');
- //    }
- //    public function suppCommentaireAction($id)
- //    {
- //        // return $this->render('ImieBlogBundle:Default:index.html.twig');
- //        return new Response('suppCommentaire'. $id);
-	// }
- //    public function afficherTousAction($page)
- //    {
- //        // return $this->render('ImieBlogBundle:Default:index.html.twig');
- //        return new Response('afficehrTous'. $page);
- //    }
+
 }
