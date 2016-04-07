db4f7683c6e832d12b3c320c22357f80bfc3c525
Shuk Ki <shuk-ki.hertz@nantes.imie.fr>
2016-04-07 16:42:45 +0200

    clean up

--- coursSymfony/README.md
+++
@@ -1,4 +0,0 @@
-coursSymfony
-============
-
-A Symfony project created on March 7, 2016, 9:24 am.
--- coursSymfony/src/Imie/TestBundle/ImieTestBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Imie\TestBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ImieTestBundle extends Bundle
-{
-}
